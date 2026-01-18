77ae269813297e5ebadef3f096f9a470e8a51d21
Artem_Mentolos <mentolosscg@yandex.ru>
2026-01-18 22:36:03 +0300

    if removal

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ export default (express, bodyParser, createReadStream, crypto, http) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', '*');
-        if (!req.path.endsWith('/')) return res.redirect(301, req.path + '/')
         next()
     })
 
