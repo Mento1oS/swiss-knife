0fceea58f662d72d070c84969f501e97aaec7efe
Artem_Mentolos <mentolosscg@yandex.ru>
2026-01-18 22:52:28 +0300

    commonjs regression

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const crypto = require('crypto')
 const http = require('http')
 const application = require('./app.js')
 
-const app = application(express, bodyParser, createReadStream, crypto, http)
-app.listen(process.env.PORT || 3000)
+const app = application(express, bodyParser, createReadStream, crypto, http);
+app.listen(process.env.PORT || 3000);
