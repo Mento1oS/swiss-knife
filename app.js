cf27730656e1c30a92a90779b7c680559006be87
Artem_Mentolos <mentolosscg@yandex.ru>
2026-01-18 21:15:11 +0300

    true initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+export default (express, bodyParser, createReadStream, crypto, http) => {
+    const app = express()
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json())
+    app.use((req, res, next) => {
+        res.set('Access-Control-Allow-Origin', '*')
+        res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE')
+        if (!req.path.endsWith('/')) return res.redirect(301, req.path + '/')
+        next()
+    })
+
+    app.get('/login/', (req, res) => {
+        res.end('c6b19a00-3764-4166-bf2b-e649083ef7a0')
+    })
+
+    app.get('/code/', (req, res) => {
+        createReadStream(import.meta.url.substring(7)).pipe(res)
+    })
+
+    app.get('/sha1/:input/', (req, res) => {
+        res.end(crypto.createHash('sha1').update(req.params.input).digest('hex'))
+    })
+
+    const handler = (req, res) => {
+        const addr = req.query.addr || req.body && req.body.addr
+        if (!addr) return res.end('')
+        http.get(addr, r => {
+            let data = ''
+            r.on('data', c => { data += c })
+            r.on('end', () => res.end(data))
+        }).on('error', () => res.end(''))
+    }
+
+    app.get('/req/', handler)
+    app.post('/req/', handler)
+
+    app.all(/.*/, (req, res) => {
+        res.end('c6b19a00-3764-4166-bf2b-e649083ef7a0')
+    })
+
+    return app
+}
