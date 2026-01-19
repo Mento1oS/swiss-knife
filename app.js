b60dbc7048cd085dea434a987a3026cf8743551a
Artem_Mentolos <mentolosscg@yandex.ru>
2026-01-20 00:54:15 +0300

    image creation

--- app.js
+++ app.js
@@ -37,6 +37,28 @@ const application = (express, bodyParser, createReadStream, crypto, http) => {
         }
     });
 
+    app.get('/makeimage', (req, res) => {
+        const width = parseInt(req.query.width, 10) || 100;
+        const height = parseInt(req.query.height, 10) || 100;
+
+        sharp({
+            create: {
+                width: width,
+                height: height,
+                channels: 4,
+                background: { r: 255, g: 255, b: 255, alpha: 1 }
+            }
+        })
+            .png()
+            .toBuffer((err, data) => {
+                if (err) {
+                    return res.status(500).send('Error generating image');
+                }
+                res.set('Content-Type', 'image/png');
+                res.send(data);
+            });
+    });
+
     app.post(
         '/decypher',
         upload.fields([
